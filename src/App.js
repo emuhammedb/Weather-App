b336a036dc5e950d21209ce9fd7ebf241aec1a2a
emhuammedb <MUENBI@users.noreply.github.com>
2021-11-09 23:33:32 +0300

    weather-app completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+// api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key}
 
-function App() {
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { usePosition } from "use-position";
+import WeatherForecast from "./components/WeatherForecast";
+
+const App = () => {
+  const [weather, setWeather] = useState();
+  const { latitude, longitude } = usePosition();
+
+  const getWeatherData = async (lat, lon) => {
+    const key = process.env.REACT_APP_WEATHER_API_KEY;
+    const lang = navigator.language.split("-")[0];
+    console.log({ lang });
+
+    try {
+      const { data } = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}&lang=${lang}&units=metric`
+      );
+      setWeather(data);
+    } catch {
+      alert("There was a problem retrieving the data. Please try again later.");
+    }
+  };
+
+  useEffect(() => {
+    latitude && longitude && getWeatherData(latitude, longitude);
+  }, [latitude, longitude]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2 class="title">Weather App</h2>
+      <WeatherForecast weather={weather} />
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/WeatherForecast.js
@@ -0,0 +1,18 @@
+const WeatherForecast = (props) => {
+  const { weather } = props;
+
+  if (!weather) {
+    return <p>You need to allow location access...</p>;
+  }
+
+  return (
+    <div>
+      <h3>{weather.name}</h3>
+      <h4>{weather.weather.map((data) => data.description).join(", ")}</h4>
+      <p>{weather.main.temp} °C</p>
+      <p>{new Date(weather.dt * 1000).toLocaleDateString()}</p>
+    </div>
+  );
+};
+
+export default WeatherForecast;
